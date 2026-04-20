159e5fe751487bd672d71a02f25bc824a8bc6625
mehmetyeyu <mehmet@yeyu.co>
2026-04-21 00:59:10 +0300

    Set timezone to Europe/Istanbul

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,8 @@
 import { Pool } from "@neondatabase/serverless";
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  options: "-c timezone=Europe/Istanbul",
+});
 
 export default pool;
