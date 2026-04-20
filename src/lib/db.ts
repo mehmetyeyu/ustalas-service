cbf087132e3f639bd32807c3f3efb0b82d5b1d8b
mehmetyeyu <mehmet@yeyu.co>
2026-04-21 01:04:00 +0300

    Revert timezone option from db pool

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,5 @@
 import { Pool } from "@neondatabase/serverless";
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  options: "-c timezone=Europe/Istanbul",
-});
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
 export default pool;
